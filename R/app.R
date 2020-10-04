3a01cca6bb276c7c573afaefeadf22499b798650
Felipe <lipecaso@gmail.com>
2020-10-04 01:33:35 -0300

    initial commit

---
+++ R/app.R
@@ -0,0 +1,30 @@
+library(RestRserve)
+library(jsonlite)
+
+
+
+model_lm <- function(data, ...) {
+  lm(mpg ~cyl, data)
+}
+
+model_handler_residuals <- function(request, response) {
+  n = jsonlite::fromJSON(request$body)
+  if (is.null(n)) {
+    raise(HTTPError$bad_request())
+  }
+  fit_model <- model_lm(n)
+
+  response$body = as.character(fit_model$residuals)
+  response$content_type = "text/plain"
+  response$headers = character(0)
+  response$status_code = 200L
+}
+
+# Creating new app
+app = Application$new(content_type = "text/plain")
+app$add_post(path = "/predict", FUN = model_handler_residuals)
+#app$add_openapi()
+
+
+backend = BackendRserve$new()
+backend$start(app, http_port = 9102)
