53a8b848bbe674d643a08f06bb444caee10d1866
Felipe <lipecaso@gmail.com>
2020-10-04 01:41:42 -0300

    change to json application

--- R/app.R
+++ R/app.R
@@ -12,19 +12,19 @@ model_handler_residuals <- function(request, response) {
   if (is.null(n)) {
     raise(HTTPError$bad_request())
   }
+
   fit_model <- model_lm(n)
 
-  response$body = as.character(fit_model$residuals)
-  response$content_type = "text/plain"
+  response$body = fit_model$residuals
+  response$content_type = "application/json"
   response$headers = character(0)
   response$status_code = 200L
 }
 
 # Creating new app
-app = Application$new(content_type = "text/plain")
+app = Application$new(content_type = "application/json")
 app$add_post(path = "/predict", FUN = model_handler_residuals)
 #app$add_openapi()
-
 
 backend = BackendRserve$new()
 backend$start(app, http_port = 9102)
